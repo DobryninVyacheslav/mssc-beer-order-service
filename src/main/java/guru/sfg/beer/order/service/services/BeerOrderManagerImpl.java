f7dd125172f6b1f2069fe9fd609b747721cab7fc
DobryninVyacheslav <w.v.dobr@gmail.com>
2021-02-23 22:12:57 +0300

    added beer order manager service

---
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManagerImpl.java
@@ -0,0 +1,54 @@
+package guru.sfg.beer.order.service.services;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEvent;
+import guru.sfg.beer.order.service.domain.BeerOrderState;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BeerOrderManagerImpl implements BeerOrderManager {
+
+    private final StateMachineFactory<BeerOrderState, BeerOrderEvent> stateMachineFactory;
+    private final BeerOrderRepository beerOrderRepository;
+
+    @Transactional
+    @Override
+    public BeerOrder newBeerOrder(BeerOrder beerOrder) {
+        beerOrder.setId(null);
+        beerOrder.setOrderStatus(BeerOrderState.NEW);
+        BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
+        sendBeerOrderEvent(savedBeerOrder, BeerOrderEvent.VALIDATE_ORDER);
+        return savedBeerOrder;
+    }
+
+    private void sendBeerOrderEvent(BeerOrder beerOrder, BeerOrderEvent event) {
+        StateMachine<BeerOrderState, BeerOrderEvent> stateMachine = build(beerOrder);
+        Message<BeerOrderEvent> message = MessageBuilder.withPayload(event).build();
+        stateMachine.sendEvent(message);
+    }
+
+    private StateMachine<BeerOrderState, BeerOrderEvent> build(BeerOrder beerOrder) {
+        StateMachine<BeerOrderState, BeerOrderEvent> stateMachine = stateMachineFactory
+                .getStateMachine(beerOrder.getId());
+
+        stateMachine.stop();
+        stateMachine.getStateMachineAccessor()
+                .doWithAllRegions(sma -> sma.resetStateMachine(
+                        new DefaultStateMachineContext<>(beerOrder.getOrderStatus(),
+                                null, null, null)
+                ));
+
+        stateMachine.start();
+
+        return stateMachine;
+    }
+}
