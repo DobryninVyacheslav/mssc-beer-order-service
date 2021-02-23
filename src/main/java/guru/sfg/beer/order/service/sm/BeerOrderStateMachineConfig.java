08f1e23bb9d14bff00b604442b424e418cad2669
DobryninVyacheslav <w.v.dobr@gmail.com>
2021-02-23 18:39:53 +0300

    delete bootstrap

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
@@ -23,5 +24,18 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                 .end(BeerOrderState.DELIVERY_EXCEPTION)
                 .end(BeerOrderState.VALIDATION_EXCEPTION)
                 .end(BeerOrderState.ALLOCATION_EXCEPTION);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<BeerOrderState, BeerOrderEvent> transitions) throws Exception {
+        transitions.withExternal()
+                .source(BeerOrderState.NEW).target(BeerOrderState.NEW)
+                .event(BeerOrderEvent.VALIDATE_ORDER)
+                .and().withExternal()
+                .source(BeerOrderState.NEW).target(BeerOrderState.VALIDATED)
+                .event(BeerOrderEvent.VALIDATION_PASSED)
+                .and().withExternal()
+                .source(BeerOrderState.NEW).target(BeerOrderState.VALIDATION_EXCEPTION)
+                .event(BeerOrderEvent.VALIDATION_FAILED);
     }
 }
